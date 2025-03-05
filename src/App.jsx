378debe9937e3f3f5a14e0c3c435f75ebf037671
Biju Kattunilathu George <biju@Bijus-MacBook-Pro.local>
2025-03-05 11:32:11 +0530

    Bug Fix for Static Pages not loading

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ import { Provider } from "react-redux";
 import appStore from "./store/appStore";
 import Connections from "./components/Connections";
 import Requests from "./components/Requests";
+import PrivacyPolicy from "./components/pages/PrivacyPolicy";
+import RefundPolicy from "./components/pages/RefundPolicy";
+import TermsOfService from "./components/pages/TermsOfService";
+import ContactUs from "./components/pages/ContactUs";
 
 function App() {
     return (
